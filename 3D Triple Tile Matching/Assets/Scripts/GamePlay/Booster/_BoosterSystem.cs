50a2fe24c09fb317ffddb844752c6e8dae5900e4
TuanDat <letuandat556@gmail.com>
2024-01-16 15:50:20 +0700

    remove shuffle from list booster

--- 3D Triple Tile Matching/Assets/Scripts/GamePlay/Booster/_BoosterSystem.cs
+++ 3D Triple Tile Matching/Assets/Scripts/GamePlay/Booster/_BoosterSystem.cs
@@ -1,7 +1,5 @@
 using System.Collections.Generic;
 using Core.Tile;
-using Cysharp.Threading.Tasks;
-using JetBrains.Annotations;
 
 namespace  Core.GamePlay.Booster
 {
@@ -9,17 +7,15 @@ namespace  Core.GamePlay.Booster
     {
            private Dictionary<_BoosterEnum, int> _dictNumOfBooster;
             private readonly _Hint _hint;
-            private readonly _Shuffle _shuffle;
+
             private readonly _Undo _undo;
 
             public _BoosterSystem(List<_TileController> listTile)
             {
                 _hint = new _Hint();
-                _shuffle = new _Shuffle();
                 _undo = new _Undo();
                 _dictNumOfBooster = new Dictionary<_BoosterEnum, int>();
                 _dictNumOfBooster.Add(_BoosterEnum.Hint, 1);
-                _dictNumOfBooster.Add(_BoosterEnum.Shuffle, 1);
                 _dictNumOfBooster.Add(_BoosterEnum.Undo, 1);
                 ListHintTileManager = new _ListHintTileManager(listTile);
             }
@@ -31,10 +27,6 @@ namespace  Core.GamePlay.Booster
                     case _BoosterEnum.Hint:
                         if(_dictNumOfBooster[_BoosterEnum.Hint] > 0)
                             _hint.Use();
-                        break;
-                    case _BoosterEnum.Shuffle:
-                        if(_dictNumOfBooster[_BoosterEnum.Shuffle] > 0)
-                            _shuffle.Use();
                         break;
                     case _BoosterEnum.Undo:
                         if(_dictNumOfBooster[_BoosterEnum.Undo] > 0)
--- 3D Triple Tile Matching/Assets/Scripts/GamePlay/UI/_UILauncher.cs
+++ 3D Triple Tile Matching/Assets/Scripts/GamePlay/UI/_UILauncher.cs
@@ -5,15 +5,12 @@ using UnityEngine.UI;
 namespace Core.UI{
     public class _UILauncher : MonoBehaviour{
         [SerializeField] private Button _hintButton;
-        [SerializeField] private Button _shuffleButton;
         [SerializeField] private Button _undoButton;
 
         private void Start(){
             _hintButton.onClick.RemoveAllListeners();
-            _shuffleButton.onClick.RemoveAllListeners();
             _undoButton.onClick.RemoveAllListeners();
             _hintButton.onClick.AddListener(() => _GameManager.Instance.BoosterSystem.UseBooster(GamePlay.Booster._BoosterEnum.Hint));
-            _shuffleButton.onClick.AddListener(() => _GameManager.Instance.BoosterSystem.UseBooster(GamePlay.Booster._BoosterEnum.Shuffle));
             _undoButton.onClick.AddListener(() => _GameManager.Instance.BoosterSystem.UseBooster(GamePlay.Booster._BoosterEnum.Undo));
         }
     }
