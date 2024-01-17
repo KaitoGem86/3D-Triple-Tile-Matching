2860118a21b044269ba901f9f904124eccd54abb
TuanDat <letuandat556@gmail.com>
2024-01-17 11:51:13 +0700

    add ContainerUI, success in setting elements

--- 3D Triple Tile Matching/Assets/Scripts/GamePlay/UI/_UILaunch.cs
+++ 3D Triple Tile Matching/Assets/Scripts/GamePlay/UI/_UILaunch.cs
@@ -1,7 +1,20 @@
 using ZBase.UnityScreenNavigator.Core;
+using ZBase.UnityScreenNavigator.Core.Activities;
+using ZBase.UnityScreenNavigator.Core.Modals;
+using ZBase.UnityScreenNavigator.Core.Screens;
 
 namespace Core.UI{
     public class _UILaunch : UnityScreenNavigatorLauncher{
+        protected override void Start()
+        {
+            base.Start();
+            SetContainer();
+        }
 
+        private void SetContainer(){
+            _ContainerUI.ActivityContainer = transform.Find("Activity").GetComponent<ActivityContainer>();
+            _ContainerUI.ModalContainer = transform.Find("Modal").GetComponent<ModalContainer>();
+            _ContainerUI.ScreenContainer = transform.Find("Screen").GetComponent<ScreenContainer>();
+        }
     }
 }
