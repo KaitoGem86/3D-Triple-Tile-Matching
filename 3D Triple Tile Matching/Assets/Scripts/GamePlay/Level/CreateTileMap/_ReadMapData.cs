ae32c465d788d7eaf3baec93c7c9a59e494194cb
TuanDat <letuandat556@gmail.com>
2024-01-22 09:09:17 +0700

    add texture

--- 3D Triple Tile Matching/Assets/Scripts/GamePlay/Level/CreateTileMap/_ReadMapData.cs
+++ 3D Triple Tile Matching/Assets/Scripts/GamePlay/Level/CreateTileMap/_ReadMapData.cs
@@ -1,4 +1,22 @@
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+
 namespace Core.Level{
-    public class _ReadMapData{
+    public class _ReadMapData : MonoBehaviour{
+        private List<Vector2> _listPosition;
+
+        [SerializeField] private Texture2D _texture2D;
+
+#if UNITY_EDITOR
+    [Button("ReadTexture")]
+        public void ReadTexture(){
+            var color = _texture2D.GetPixels();
+            Debug.Log(_texture2D.width + " " + _texture2D.height);
+            foreach (var c in color){
+                Debug.Log(c);
+            }
+        }
+#endif
     }
 }
